fb75182afad51cbd1f375c4f7675623a1495fd32
779337617 <yyang18@wpi.edu>
2019-01-14 20:32:19 -0500

    1

---
+++ sample(1)/Assets/Script/ironhand.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ironhand : MonoBehaviour {
+    Rigidbody2D rb2d;
+    Vector2 pushdirection;
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mouseposition = Input.mousePosition;
+        Vector3 handposition =Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 direction = mouseposition-handposition;
+        direction.z = 0f;
+        direction = direction.normalized;
+        float pushangel = Mathf.Atan(direction.y / direction.x);
+        float pushdegree = Mathf.Rad2Deg*pushangel-90;
+        transform.rotation = Quaternion.AngleAxis(pushdegree, Vector3.forward);  
+        
+        if(Input.GetMouseButtonDown(1))
+        {
+            pushdirection.x = Mathf.Cos(pushangel);
+            pushdirection.y = Mathf.Sin(pushangel);
+            rb2d.AddForce(50 * pushdirection, ForceMode2D.Impulse);
+        }
+    }
+    public void pushing(vecto)
+}
---
+++ sample(1)/Assets/Script/player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+    Rigidbody2D rb2d;
+    int playerMoveUnitsPerSecond;
+
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        playerMoveUnitsPerSecond = 20;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput= Input.GetAxis("Vertical");
+        if ( horizontalInput != 0|| verticalInput != 0)
+        {
+            Vector2 position = rb2d.position;
+            position.x += horizontalInput * playerMoveUnitsPerSecond *
+                Time.deltaTime;
+            position.y += verticalInput * playerMoveUnitsPerSecond *
+                Time.deltaTime;
+            rb2d.MovePosition(position);
+        }
+        
+    }
+
+}
