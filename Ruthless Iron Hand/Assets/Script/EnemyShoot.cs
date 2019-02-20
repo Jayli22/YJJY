1595a264e03a80a0b6b07d8692e652c7d326cfb9
jayli22 <lijiayi_jay@outlook.com>
2019-02-20 12:52:04 -0500

    Update

--- Ruthless Iron Hand/Assets/Script/EnemyShoot.cs
+++ Ruthless Iron Hand/Assets/Script/EnemyShoot.cs
@@ -7,7 +7,10 @@ public class EnemyShoot : MonoBehaviour
     private Vector2 landPosition;
     private Vector2 startPosition;
     private Timer changeTimer;
+    public GameObject targetPrefab;
     private float t;
+    private bool isTarget = false;
+    private GameObject target;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,12 +19,20 @@ public class EnemyShoot : MonoBehaviour
         changeTimer.Run();
         startPosition = transform.position;
         t = 0;
+        targetPrefab = Resources.Load("Target") as GameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(changeTimer.Finished)
+
+        if (!isTarget)
+        {
+            target = Instantiate(targetPrefab, landPosition, transform.rotation);
+            isTarget = true;
+
+        }
+        if (changeTimer.Finished)
         {
             changeTimer.Run();
             transform.position =  Vector2.Lerp(startPosition, landPosition, t);
@@ -31,6 +42,7 @@ public class EnemyShoot : MonoBehaviour
         if(t >= 1)
         {
             Destroy(GetComponent<EnemyShoot>());
+            Destroy(target);
         }
     }
 
