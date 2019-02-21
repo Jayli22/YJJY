50dfd7b236b9de3a0dbae75346dde595ac6e8b25
779337617 <yyang18@wpi.edu>
2019-02-20 23:25:28 -0500

    update220
    add ui

--- Ruthless Iron Hand/Assets/Script/EnemyShoot.cs
+++ Ruthless Iron Hand/Assets/Script/EnemyShoot.cs
@@ -25,30 +25,18 @@ public class EnemyShoot : MonoBehaviour
         {
             changeTimer.Run();
             transform.position =  Vector2.Lerp(startPosition, landPosition, t);
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
->>>>>>> parent of 62220435... Wrong
-=======
->>>>>>> parent of 62220435... Wrong
+
+
             t += 0.01f;
             //Debug.Log(t);
         }
         if(t >= 1)
         {
             Destroy(GetComponent<EnemyShoot>());
-<<<<<<< HEAD
-<<<<<<< HEAD
-            Destroy(target);
-=======
->>>>>>> parent of 62220435... Wrong
-=======
->>>>>>> parent of 62220435... Wrong
-=======
-            t += 0.05f;
->>>>>>> parent of fe1c40ec... Merge branch 'DevJy' of https://github.com/Jayli22/YJJY into DevJy
+
+          //  Destroy(target);
+
         }
     }
 
